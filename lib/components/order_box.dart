1116fb0ddf8ce713183fd50365212a8c6cf2be5c
Akintayo Toheeb <akintayo.t2002@gmail.com>
2023-02-25 11:51:25 +0100

    Created Orders screen

--- lib/components/order_box.dart
+++ lib/components/order_box.dart
@@ -1,12 +1,21 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../providers/order_provider.dart';
 
-class OrderBox extends StatelessWidget {
-  const OrderBox({super.key,required this.order,});
+class OrderBox extends StatefulWidget {
+  OrderBox({super.key,required this.order,});
 
   final OrderItem order;
+
+  @override
+  State<OrderBox> createState() => _OrderBoxState();
+}
+
+class _OrderBoxState extends State<OrderBox> {
+  bool _isExpanded=false;
 
   @override
   Widget build(BuildContext context) {
@@ -16,40 +25,50 @@ class OrderBox extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
       ),
       padding: const EdgeInsets.all(15),
-      child: Row(
+      child: Column(
         children: [
-          Image.asset(
-            'assets/images/airpod3.png',
-            height: 75,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children:const[
-                Text(
-                'Airpod 3',
-                style: TextStyle(color: Colors.white, fontSize: 17),
+          Row(
+            mainAxisAlignment:MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment:CrossAxisAlignment.start,
+                children: [
+                  Text(DateFormat('dd MMMM yyyy').format(widget.order.dateTime),style:const TextStyle(
+                    color:Colors.white,
+                    fontSize:18,
+                  ),),
+                  const SizedBox(height:5,),
+                  Text('Total: \$${widget.order.amount}',style:const TextStyle(
+                    fontSize:16,
+                    color:Colors.green,
+                  ),)
+                ],
               ),
-              Text(
-                '3 items',
-               style: TextStyle(color:Colors.white, fontSize: 16),
-              ),
+             GestureDetector(
+          onTap:(){
+            setState(() {
+              _isExpanded=!_isExpanded;
+            });
+          },
+          child:Icon(_isExpanded?Icons.expand_less:Icons.expand_more,color:Colors.white,size:35,)),
             ],
           ),
-          const Spacer(),
-          Column(
-            children:[
-              Text('\$${order.amount}',style:const TextStyle(
-                color:Colors.green,
-                fontSize:18,
-              ),),
-              Text(DateFormat('d MMMM y').format(order.dateTime),style:TextStyle(
-                color:Colors.grey.shade600,
-                fontSize:14,
-              ),),
-            ],
-          )
+          if(_isExpanded)Container(
+               width:double.infinity,
+              height:min(widget.order.products.length*20+20,180),
+              padding: const EdgeInsets.all(5),
+              child:ListView(
+                children:widget.order.products.map((ord) =>Row(
+                  mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(ord.title,style:const TextStyle(color:Colors.white,fontSize:16),),
+                    Text('\$${ord.price.toStringAsFixed(0)}',style:const TextStyle(color:Colors.green),)
+                  ],
+                )).toList(),
+              ),
+             ),
         ],
-      ),
+      )
     );
   }
 }
